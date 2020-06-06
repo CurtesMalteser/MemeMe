292fb430eba2720d431293e941eed01fcdb7fe0c
Antonio <antonio@igenius.ai>
2020-06-06 18:55:07 +0200

    renamed ViewController, added collection and table VC to display sent Memes

---
+++ MemeMe/Controllers/SentMemesCollectionVC.swift
@@ -0,0 +1,9 @@
+//
+//  SentMemesCollectionVC.swift
+//  MemeMe
+//
+//  Created by Antonio on 06.06.20.
+//  Copyright © 2020 CurtesMalteser. All rights reserved.
+//
+
+import Foundation
---
+++ MemeMe/Controllers/SentMemesTableVC.swift
@@ -0,0 +1,9 @@
+//
+//  SentMemesTableVC.swift
+//  MemeMe
+//
+//  Created by Antonio on 06.06.20.
+//  Copyright © 2020 CurtesMalteser. All rights reserved.
+//
+
+import Foundation
