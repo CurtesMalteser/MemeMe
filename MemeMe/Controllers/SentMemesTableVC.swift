c543e07438ef69b0a86cf182cf0fd6279836e752
Antonio <antonio@igenius.ai>
2020-06-12 21:39:32 +0200

    added basic implementation of SentMemesTableVC

--- MemeMe/Controllers/SentMemesTableVC.swift
+++ MemeMe/Controllers/SentMemesTableVC.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
-class SentMemesTableVC: UIViewController {
+class SentMemesTableVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var memes: [Meme]! {
         let object = UIApplication.shared.delegate
         let appDelegate = object as! AppDelegate
         return appDelegate.memes
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeCell")!
+        let meme = memes[(indexPath as NSIndexPath).row]
+        
+        // Set the name and image
+        cell.textLabel?.text = meme.topText
+        cell.imageView?.image = meme.memedImage
+        
+        // If the cell has a detail label, we will put the evil scheme in.
+        // if let detailTextLabel = cell.detailTextLabel {
+        // detailTextLabel.text = "Scheme: \(villain.evilScheme)"
+        // }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let meme = memes[(indexPath as NSIndexPath).row]
+        print("meme: \(meme)")
     }
     
 }
