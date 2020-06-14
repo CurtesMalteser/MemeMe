5710f09390ec431fc76848c7dc7b5759a4e4e853
Antonio <antonio@igenius.ai>
2020-06-14 15:42:38 +0200

    added MemeDetailVC

---
+++ MemeMe/Controllers/MemeDetailVC.swift
@@ -0,0 +1,13 @@
+//
+//  MemeDetailVC.swift
+//  MemeMe
+//
+//  Created by Antonio on 14.06.20.
+//  Copyright © 2020 CurtesMalteser. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailVC: UIViewController {
+    
+}
